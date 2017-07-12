8a8559cd48e3261f4268d00cc36aea9db5b57257
Diana Ibarra <dibarra88@gmail.com>
2017-07-12 15:15:54 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+console.log(customers.results);
+
+let profileDiv = "";
+
+customers.results.forEach(function (profile) {
+
+    profileDiv += `
+    <div class="profile">
+        <img src="${profile.picture.large}">
+        <h3>${profile.name.first.toUpperCase()} ${profile.name.last.toUpperCase()}</h3>
+        <div class="info">
+            <h6 class="email">${profile.email.toUpperCase()}</h6>
+            <address>
+                ${profile.location.street}
+                ${profile.location.city}, ${profile.location.state} ${profile.location.postcode}</br>
+                ${profile.cell}
+            </address>
+            <div class="ssn">
+                ${profile.id.value}
+            </div>
+        </div>
+    
+    </div>`
+
+    console.log(profileDiv)
+});
+
+document.querySelector(".profilesContainer").innerHTML = profileDiv;
